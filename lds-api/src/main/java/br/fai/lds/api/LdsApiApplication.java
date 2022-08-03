1088ed94fed39459bdcfdc9eadc018a9a558b460
enthony <enthonyedu@gmail.com>
2022-08-02 22:35:57 -0300

    feat: projeto base

---
+++ lds-api/src/main/java/br/fai/lds/api/LdsApiApplication.java
@@ -0,0 +1,12 @@
+package br.fai.lds.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LdsApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LdsApiApplication.class, args);
+    }
+}
---
+++ lds-client/src/test/java/br/fai/lds/client/LdsClientApplicationTests.java
@@ -0,0 +1,13 @@
+package br.fai.lds.client;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class LdsClientApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
