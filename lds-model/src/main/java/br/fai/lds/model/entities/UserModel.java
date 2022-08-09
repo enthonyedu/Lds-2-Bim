0e01b5b325e4bdffaf4adf6f9c3998628beb14f0
enthony <enthonyedu@gmail.com>
2022-08-08 22:47:21 -0300

    feat: adição do model UserModel

---
+++ lds-model/src/main/java/br/fai/lds/model/entities/UserModel.java
@@ -0,0 +1,78 @@
+package br.fai.lds.model.entities;
+
+import br.fai.lds.model.enums.UserType;
+
+public class UserModel extends BaseEntity {
+    private int id;
+    private String username;
+    private String password;
+    private String fullName;
+    private String email;
+    private UserType type;
+    private boolean isActive;
+    private byte[] avatar;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getFullName() {
+        return fullName;
+    }
+
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public UserType getType() {
+        return type;
+    }
+
+    public void setType(UserType type) {
+        this.type = type;
+    }
+
+    public boolean isActive() {
+        return isActive;
+    }
+
+    public void setActive(boolean active) {
+        isActive = active;
+    }
+
+    public byte[] getAvatar() {
+        return avatar;
+    }
+
+    public void setAvatar(byte[] avatar) {
+        this.avatar = avatar;
+    }
+}
