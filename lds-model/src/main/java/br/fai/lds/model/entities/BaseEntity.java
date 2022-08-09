dcd0495e6248bc2d179f4114b7addd9fa9ecadc5
enthony <enthonyedu@gmail.com>
2022-08-08 22:46:29 -0300

    feat: adição do BaseEntity

---
+++ lds-model/src/main/java/br/fai/lds/model/entities/BaseEntity.java
@@ -0,0 +1,42 @@
+package br.fai.lds.model.entities;
+
+import java.sql.Timestamp;
+
+public class BaseEntity {
+    private Timestamp createdAt;
+    private String createdBy;
+    private Timestamp lastModified;
+    private String lasModifiedBy;
+
+    public Timestamp getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(Timestamp createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public String getCreatedBy() {
+        return createdBy;
+    }
+
+    public void setCreatedBy(String createdBy) {
+        this.createdBy = createdBy;
+    }
+
+    public Timestamp getLastModified() {
+        return lastModified;
+    }
+
+    public void setLastModified(Timestamp lastModified) {
+        this.lastModified = lastModified;
+    }
+
+    public String getLasModifiedBy() {
+        return lasModifiedBy;
+    }
+
+    public void setLasModifiedBy(String lasModifiedBy) {
+        this.lasModifiedBy = lasModifiedBy;
+    }
+}
