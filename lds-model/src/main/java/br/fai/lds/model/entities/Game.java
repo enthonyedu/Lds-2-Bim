92f10ab996ac0be8c6f6e850b2d5a7ea2ac3604c
enthony <enthonyedu@gmail.com>
2022-08-09 21:24:43 -0300

    feat: adição do restante dos models

---
+++ lds-model/src/main/java/br/fai/lds/model/entities/Game.java
@@ -0,0 +1,70 @@
+package br.fai.lds.model.entities;
+
+import br.fai.lds.model.enums.GameType;
+
+public class Game extends BaseEntity {
+
+    private int id;
+    private GameType type;
+    private String name;
+    private String description;
+    private double price;
+    private String developedBy;
+    private String releaseDate;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public GameType getType() {
+        return type;
+    }
+
+    public void setType(GameType type) {
+        this.type = type;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public String getDevelopedBy() {
+        return developedBy;
+    }
+
+    public void setDevelopedBy(String developedBy) {
+        this.developedBy = developedBy;
+    }
+
+    public String getReleaseDate() {
+        return releaseDate;
+    }
+
+    public void setReleaseDate(String releaseDate) {
+        this.releaseDate = releaseDate;
+    }
+}
