cbdf7030ef4322196391cabf9e2c897fcca85f6d
enthony <enthonyedu@gmail.com>
2022-08-15 22:42:24 -0300

    feat: adicionado restService

---
+++ lds-client/src/main/java/br/fai/lds/client/service/impl/RestServiceImpl.java
@@ -0,0 +1,19 @@
+package br.fai.lds.client.service.impl;
+
+import br.fai.lds.client.service.RestService;
+import org.springframework.http.HttpHeaders;
+import org.springframework.stereotype.Service;
+
+@Service
+public class RestServiceImpl implements RestService {
+
+    @Override
+    public HttpHeaders getAuthenticatedHeaders(String username, String password) {
+        return null;
+    }
+
+    @Override
+    public HttpHeaders getRequestHeaders() {
+        return null;
+    }
+}
