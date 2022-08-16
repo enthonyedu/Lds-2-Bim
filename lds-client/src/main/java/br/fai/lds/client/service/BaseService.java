17efcc073820c0a2f19bdae63b52ebb64deba3c8
enthony <enthonyedu@gmail.com>
2022-08-15 22:32:51 -0300

    feat: adicionado interface base

---
+++ lds-client/src/main/java/br/fai/lds/client/service/BaseService.java
@@ -0,0 +1,16 @@
+package br.fai.lds.client.service;
+
+import java.util.List;
+
+public interface BaseService<T> {
+
+    int create(T entity);
+
+    List<T> find();
+
+    T findById(int id);
+
+    boolean update(T entity);
+
+    boolean deleteById(int id);
+}
