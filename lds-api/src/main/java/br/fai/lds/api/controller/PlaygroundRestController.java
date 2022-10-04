3b0a1f0c7c09b0e5f536f6c71c021a27d42eaf34
enthony <enthonyedu@gmail.com>
2022-10-03 22:03:49 -0300

    feat: adicao do swagger no projeto

--- lds-api/src/main/java/br/fai/lds/api/controller/PlaygroundRestController.java
+++ lds-api/src/main/java/br/fai/lds/api/controller/PlaygroundRestController.java
@@ -16,4 +16,10 @@ public class PlaygroundRestController {
 
         return ResponseEntity.ok("boa noite =D");
     }
+
+    @GetMapping("/openapi-test")
+    public ResponseEntity<String> getOpenApi() {
+
+        return ResponseEntity.ok("teste com o OpenApi");
+    }
 }
