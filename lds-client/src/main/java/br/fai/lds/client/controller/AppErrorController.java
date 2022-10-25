d07821be4561610b193e35691e1209b7a07811be
Enthony <enthonyedu@gmail.com>
2022-10-24 21:51:33 -0300

    feat: adicao do AppErrorController

---
+++ lds-client/src/main/java/br/fai/lds/client/controller/AppErrorController.java
@@ -0,0 +1,17 @@
+package br.fai.lds.client.controller;
+
+import org.springframework.boot.web.servlet.error.ErrorController;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class AppErrorController  implements ErrorController {
+
+    private static final String PATH = "/error";
+
+    @RequestMapping(value = PATH)
+    public String error(){
+        return "forward:/common/not-found";
+    }
+
+}
