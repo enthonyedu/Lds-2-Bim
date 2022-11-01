46ba1076527271dc3071fc940130269914cb004d
Enthony <enthonyedu@gmail.com>
2022-10-31 22:39:06 -0300

    feat: adicao do filtro JWT no projeto API

---
+++ lds-api/src/main/java/br/fai/lds/api/security/RestSecurityConfig.java
@@ -0,0 +1,23 @@
+package br.fai.lds.api.security;
+
+import br.fai.lds.api.security.filter.JwtAuthorizationFilter;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+@EnableWebSecurity
+@Configuration
+public class RestSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity httpSecurity) throws Exception {
+        httpSecurity
+                .csrf().disable()
+
+                .addFilterAfter(new JwtAuthorizationFilter, UsernamePasswordAuthenticationFilter.class)
+                .authorizeRequests()
+                .anyRequest().authenticated();
+    }
+}
